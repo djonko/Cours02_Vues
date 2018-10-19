c3079f747b9154c59c8f587dd48312162e5648c4
djonko <prevostdjonko@gmail.com>
2018-10-19 00:19:41 -0400

    Projet mvc partie 2

---
+++ Cours02_Vues/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cours02_Vues.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
